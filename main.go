b191d4478b39b5c06840d8da959ccce4a34e7828
tayefulislam <kazitayefulislam12@gmail.com>
2022-07-04 16:17:21 +0600

    MY Go Lang 1st project

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// fmt.Print("Tayeful Islam \nClass :12 \nRoll :34\n")
+	// fmt.Println("Islam \t\t fffkfs\\i")
+	// fmt.Printf("d")
+	// fmt.Print("\"Tayeful\"")
+
+	// initailize the variables
+	// var name, district string
+	// var roll int32
+
+	// set vale;
+	var name = "Tayeful"
+
+	var roll = 34
+	var district = "Brahmanbaria"
+
+	// new stror cart
+
+	zila := "Nabinagar"
+
+	fmt.Println(name)
+	fmt.Println("Roll : ", roll)
+	fmt.Println("District : ", district, "Name", name, zila)
+
+}
